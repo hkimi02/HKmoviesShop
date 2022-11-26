6f1072a541559005b10cb4e27e96dc1e88d667a5
hkimi amin <hkimiamin02@gmail.com>
2022-11-26 16:13:58 +0100

    update profiles

---
+++ profile_employe/index.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once './db_connect.php';
+    session_start();
+    include './home.phtml';
+
+?> 
---
+++ profile_user/index.php
@@ -0,0 +1,5 @@
+<?php 
+    require_once '../db_connect.php';
+    session_start();
+    include './home.phtml';
+?> 
