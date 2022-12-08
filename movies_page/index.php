d47c0f60b7a1e15dd68e9101332a477e831d8955
hkimi amin <hkimiamin02@gmail.com>
2022-12-08 13:22:55 +0100

    ..update movies page

--- movies_page/index.php
+++ movies_page/index.php
@@ -1,4 +1,9 @@
 <?php 
     require_once '../db_connect.php';
+    session_start();
+    if(isset($_SESSION['username'])){
     include './home.phtml';
+    }else{
+        header('location:../index.php?msg=you have to log in to acces movies page&class=danger');
+    }
 ?>
