ddf59f0b5a44b73f6d7ae411bfbb4083a7624de6
hkimi amin <hkimiamin02@gmail.com>
2022-12-09 01:19:29 +0100

    add like movie add like show

--- movies_page/index.php
+++ movies_page/index.php
@@ -23,6 +23,32 @@
         if(array_key_exists('filter',$_GET)){
             $filter=$_GET['filter'];
         }
+        if(array_key_exists('id_fav_movie',$_GET)){
+            $id_movie=$_GET['id_fav_movie'];
+            $req=$db->prepare('INSERT INTO likes(id_user,id_movie,garbage) VALUES(:id_user,:id_movie,:garbage)');
+            $req->execute([
+                'id_user'=>$_SESSION['iduser'],
+                'id_movie'=>$id_movie,
+                'garbage'=>0,
+            ]
+        );
+            if($req){
+                header('location:index.php?msg=movie added succesfuly to your favourite list&class=success');
+            }
+        }
+        if(array_key_exists('id_fav_show',$_GET)){
+            $id_show=$_GET['id_fav_show'];
+            $req=$db->prepare('INSERT INTO likes_shows(id_user,id_show,garbage) VALUES(:id_user,:id_show,:garbage)');
+            $req->execute([
+                'id_user'=>$_SESSION['iduser'],
+                'id_show'=>$id_show,
+                'garbage'=>0,
+            ]
+        );
+            if($req){
+                header('location:index.php?msg=show added succesfuly to your favourite list&class=success');
+            }
+        }
     include './home.phtml';
     }else{
         header('location:../index.php?msg=you have to log in to acces movies page&class=danger');
