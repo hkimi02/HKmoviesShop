768364bab5860118dc9af20cc6c89a9d85b71c0c
hkimi amin <hkimiamin02@gmail.com>
2022-12-10 01:28:16 +0100

    update

--- admin/index.php
+++ admin/index.php
@@ -30,8 +30,49 @@
         $res=$req->fetch();
         return $res['nb_employe'];
     }
+    function calculer_taille($nbminutes){
+        $movie['nbheures']=((int)($nbminutes/60)).'h';
+        $movie['nbminutes']=($nbminutes%60).'min';
+        return $movie;
+    }
+    //deteremine genre of every movie starting from the forgein key on the table 
+    function donner_genre($id_genre,$db){
+        $req=$db->prepare('SELECT namegenre FROM genre WHERE idgenre=:id_genre');
+        $req->execute(['id_genre'=>$id_genre]);
+        $added_genre=$req->fetchAll();
+        foreach($added_genre as $value){
+        echo $value['namegenre']; 
+    }
+}
     if(isset($_SESSION['username']) && $_SESSION['isAdmin']==1){
-    //en cours demandes
+        //select liked movies not deleted
+        $req=$db->prepare('SELECT m.* FROM movies m,likes l WHERE l.id_user=:id_user AND l.id_movie=m.idmovie AND l.garbage=0');
+    $req->execute(['id_user'=>$_SESSION['iduser']]);
+$liked_movies=$req->fetchAll();
+// var_dump($liked_movies);
+// exit;
+//select liked shows not deleted
+$req=$db->prepare('SELECT tv.* FROM tvshows tv,likes_shows l WHERE l.id_user=:id_user AND l.id_show=tv.idtvshow AND l.garbage=0');
+$req->execute(['id_user'=>$_SESSION['iduser']]);
+$liked_shows=$req->fetchAll();
+// var_dump($liked_shows);
+
+//delete for ever movie from likes
+if(array_key_exists('id_forever_movie',$_GET)){
+    $req=$db->prepare('DELETE FROM likes WHERE id_movie=:idmovie and id_user=:id_user');
+    $req->execute(['idmovie'=>$_GET['id_forever_movie'],'id_user'=>$_SESSION['iduser']]);
+    header('location:index.php?msg=movie deleted&class=success');
+}
+//shows
+//delete forever show
+if(array_key_exists('id_forever_show',$_GET)){
+    $req=$db->prepare('DELETE FROM likes_shows WHERE id_show=:idshow and id_user=:id_user');
+    $req->execute(['idshow'=>$_GET['id_forever_show'],'id_user'=>$_SESSION['iduser']]);
+    header('location:index.php?msg=show deleted&class=success');
+}
+
+// exit;
+        //en cours demandes
     $req=$db->prepare('SELECT * FROM jobrequest WHERE STATE=0'); 
     $req->execute();
     $pending=$req->fetchAll();
